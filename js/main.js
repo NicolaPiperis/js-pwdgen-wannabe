25aba48cbbd722ef0434ecd88968f60962234c25
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-04-26 13:24:43 +0200

    js flowchart

---
+++ js/main.js
@@ -0,0 +1,19 @@
+    // Dichiara le variabili
+
+
+    // Chiedi all’utente il suo nome
+
+
+    // Poi chiedi il suo cognome
+
+
+    // Poi chiedi il suo colore preferito
+
+
+
+    // Crea il messaggio
+
+
+    // Infine scrivi sulla pagina nomecognomecolorepreferito+numero
+
+    
