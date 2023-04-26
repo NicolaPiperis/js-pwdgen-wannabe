eb30232738b29e5141489e716871bbd028e9f4a7
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-04-26 15:07:10 +0200

    html, css, js exercise completed

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-    // Dichiara le variabili
+    // Declare the variables
+    let userName, userSurname, userFavoriteColor, password, message;
 
+    // Ask the name of the user
+    userName = prompt("Insert your name");
+    console.log(userName);
+    
+    // Then the surname
+    userSurname = prompt("Insert your surname");
+    console.log(userSurname);
 
-    // Chiedi all’utente il suo nome
+    // Then the favorite color
+    userFavoriteColor = prompt("Insert your favorite color");
+    console.log(userFavoriteColor);
 
+    // Build the password
+    password = userName + userSurname + userFavoriteColor + 23;
+    console.log(password);
 
-    // Poi chiedi il suo cognome
+    // Make the message
+    message = `The best password for you is ` + password;
+    console.log(message);
 
-
-    // Poi chiedi il suo colore preferito
-
-
-
-    // Crea il messaggio
-
-
-    // Infine scrivi sulla pagina nomecognomecolorepreferito+numero
-
+    // Finally write on the page name+surname+favoritecolor+23
+    document.getElementById("mio_id").innerHTML = message
     
